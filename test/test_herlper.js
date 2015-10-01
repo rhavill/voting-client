dcbc913221aa8bea5fd142a60ae5a6b50c17aa4f
Ricardo Havill <rhavill2002@yahoo.com>
2015-09-30 21:05:48 -0400

    Install and use immutable and chai-immutable.

--- test/test_herlper.js
+++ test/test_herlper.js
@@ -1,4 +1,6 @@
 import jsdom from 'jsdom';
+import chai from 'chai';
+import chaiImmutable from 'chai-immutable';
 
 const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
 const win = doc.defaultView;
@@ -7,7 +9,9 @@ global.document = doc;
 global.window = win;
 
 Object.keys(window).forEach((key) => {
-    if (!(key in global)) {
-        global[key] = window[key];
-    }
+  if (!(key in global)) {
+    global[key] = window[key];
+  }
 });
+
+chai.use(chaiImmutable);
