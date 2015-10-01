774f42265de06de59740713b66db391581362d75
Ricardo Havill <rhavill2002@yahoo.com>
2015-10-01 18:55:07 -0400

    Disable buttons after a vote and display a winner after voting ends.

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,6 @@ import Voting from './components/Voting';
 const pair = ['Trainspotting', '28 Days Later'];
 
 React.render(
-    <Voting pair={pair} />,
+    <Voting pair={pair} winner="Trainspotting" />,
     document.getElementById('app')
 );
