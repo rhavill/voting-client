894ffbb1494f8865320a3a40fa96cdd18c3f08a1
Ricardo Havill <rhavill2002@yahoo.com>
2015-09-30 21:54:31 -0400

    Added first react component.

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,9 @@
-console.log('I am aliver!');
+import React from 'react';
+import Voting from './components/Voting';
+
+const pair = ['Trainspotting', '28 Days Later'];
+
+React.render(
+    <Voting pair={pair} />,
+    document.getElementById('app')
+);
