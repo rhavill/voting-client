1e7b550984fb4c4e074f2b7a825af3340c38c926
Ricardo Havill <rhavill2002@yahoo.com>
2015-10-01 22:54:07 -0400

    Implemented results page, which shows vote tally.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import {List} from 'immutable';
+import {List, Map} from 'immutable';
 
 const pair = List.of('Trainspotting', '28 Days Later');
+const tally = Map({'Trainspotting': 5, '28 Days Later': 4});
 
 export default React.createClass({
     render: function() {
         return (
             <div>
                 <h1>voting client</h1>
-                {React.cloneElement(this.props.children, {pair: pair })}
+                {React.cloneElement(this.props.children, {pair: pair, tally: tally })}
             </div>
         )
     }
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -2,9 +2,25 @@ import React from 'react/addons';
 
 export default React.createClass({
     mixins: [React.addons.PureRenderMixin],
+    getPair: function() {
+        return this.props.pair || [];
+    },
+    getVotes: function(entry) {
+        if (this.props.tally && this.props.tally.has(entry)) {
+            return this.props.tally.get(entry);
+        }
+        return 0;
+    },
     render: function() {
-        return <div>
-            <h2>Blah</h2>
+        return <div className="results">
+            {this.getPair().map(entry =>
+                    <div key={entry} className="entry">
+                        <h1>{entry}</h1>
+                        <div className="voteCount">
+                            {this.getVotes(entry)}
+                        </div>
+                    </div>
+            )}
         </div>;
     }
 });
