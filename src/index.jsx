1c9947d995327eaa6458a29c335fccb972ba4a58
Ricardo Havill <rhavill2002@yahoo.com>
2015-10-04 17:37:33 -0400

    Added a redux store and reducer fuction to set the state. Also added tests for SET_STATE.

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,19 @@ import App from './components/App';
 import Results from './components/Results';
 import Voting from './components/Voting';
 import { Router, Route, IndexRoute } from 'react-router';
+import {createStore} from 'redux';
+import reducer from './reducer';
+
+const store = createStore(reducer);
+store.dispatch({
+    type: 'SET_STATE',
+    state: {
+        vote: {
+            pair: ['Sunshine', '28 Days Later'],
+            tally: {Sunshine: 2}
+        }
+    }
+});
 
 React.render((
     <Router>
